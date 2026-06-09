d5b346c0fb77dc55173d0678921ae8d15accb69b
PlotChat <levietanh1234567@gmail.com>
2026-06-09 11:40:41 +0700

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<div className="d-flex flex-column text-center py-5 bg-black text-white">
 			<div>Student Name: Le Viet Anh</div>
-			<div>Studnet id: HE200455</div>
+			<div>Student id: HE200455</div>
 			<div>Class name: SE2004</div>
 			<div>Email address: levietanh1234567@gmail.com</div>
 		</div>
